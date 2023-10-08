aed061420d920b69f8ef9321d9ff07a87fac65c9
lemz <ismael.amjad07@gmail.com>
2023-10-08 20:40:22 +0200

    premake

---
+++ premake5.lua
@@ -0,0 +1,29 @@
+workspace "Ray Marching"
+    architecture "x64"
+    startproject "Ray Marching"
+    configurations { "Debug", "Release" }
+
+    outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.arctitecture}"
+
+    project "Ray Marching"
+        location "Ray Marching" -- this line sets the location for the generated project files
+        kind "ConsoleApp"
+        language "C++"
+
+        targetdir("bin/"..outputdir.."/%{prj.name}")
+        objdir("bin-int/"..outputdir.."/%{prj.name}")
+
+        files { "**.h", "**.cpp", "**.c" } -- this line sets the relative path to the source files
+
+        includedirs {"Ray Marching/libraries/include"} -- this line sets the path to the include files
+        libdirs {"Ray Marching/libraries/lib"} -- this line sets the path to the library files
+
+        links {"glfw3"} -- this line links the GLFW library
+
+        filter "configurations:Debug"
+            defines { "DEBUG" }
+            symbols "On"
+
+        filter "configurations:Release"
+            defines { "NDEBUG" }
+            optimize "On"
---
+++ setup.bat
@@ -0,0 +1,5 @@
+@echo off
+
+call premake\premake5.exe vs2022
+
+PAUSE
